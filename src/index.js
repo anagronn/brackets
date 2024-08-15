f26f1c31fed19b79eb8595b1e4ec105bde9a0f64
anagronn <nastasya.vodchits@mail.ru>
2024-08-15 20:43:58 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openedBrackets = [];
+  for (let char of str) {
+    for (let [opened, closed] of bracketsConfig) {
+      if (char === closed && openedBrackets[openedBrackets.length - 1] === opened) {
+        openedBrackets.pop(); 
+      } else if (char === opened) {
+        if (opened === closed && openedBrackets[openedBrackets.length - 1] === opened) {
+          openedBrackets.pop(); 
+        } else {
+          openedBrackets.push(char);
+        }
+      } else if (char === closed && openedBrackets.length === 0) {
+        return false;
+      }
+    }
+  }
+  return openedBrackets.length === 0;
 }
